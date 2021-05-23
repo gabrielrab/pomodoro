15722709d499618725fee2671ac25c0cf2ef9ed7
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-05-23 00:10:21 -0300

    chore: basic example notification

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from "react";
+
 function App() {
-  return <>Only Electron app runing using react</>;
+  const notify = () => {
+    new Notification("Try send notification", {
+      body: "Yes, you can send notofication",
+    });
+  };
+
+  return (
+    <>
+      Only Electron app runing using react{" "}
+      <button onClick={notify}>Click</button>
+    </>
+  );
 }
 
 export default App;
