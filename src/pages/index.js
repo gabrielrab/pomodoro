90a576ebda91cc70fceeec46dc8b855d7de157d0
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-12-27 00:12:16 -0300

    chore: init timer

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,29 @@ import Layout from "@components/Layout";
 
 import useTimer from "@hooks/useTimer";
 
+// // Requisitos
+// A cada 1 session - 5 min de descanso
+// A cada 4 sessions - 1 stop (stop session)
+
+//  // Dados
+// Stop session - 30 min
+// Session interval - 5 min
+
+//  // Ideias extras
+// Quando estiver em tempo de working, apresentar uma animação de uma pessoa codando
+// Colocar abaixo da imagem uma barra de progresso
+// Colocar o horário que vai terminar a sessão
+
 export default function Index() {
-  const { session } = useTimer();
+  const { reset, min, sec } = useTimer();
   return (
     <Layout>
       Hello World, here its index page!
       <br />
-      Timer: {session}
+      Timer: {min}:{sec}
+      <br />
+      {/* <button onClick={start}>Start</button> */}
+      <button onClick={reset}>Reset</button>
     </Layout>
   );
 }
--- src/providers/TimerProvider.js
+++ src/providers/TimerProvider.js
@@ -1,17 +1,48 @@
-import React, { createContext, useMemo, useState } from "react";
+import React, {
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 
 const TimerContext = createContext();
 
 export default function TimerProvider({ children }) {
-  const [session, setSession] = useState(1);
+  const timer = { hour: 0, minute: 0, second: 60 };
+  const [[hr, min, sec], setTime] = useState([
+    timer.hour,
+    timer.minute,
+    timer.second,
+  ]);
 
-  const value = useMemo(
-    () => ({
-      session,
-      setSession,
-    }),
-    [session, setSession]
+  useEffect(() => {
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
+
+  const tick = () => {
+    if (hr === 0 && min === 0 && sec === 0) reset();
+    else if (min === 0 && sec === 0) {
+      setTime([hr - 1, 59, 59]);
+    } else if (sec === 0) {
+      setTime([hr, min - 1, 59]);
+    } else {
+      setTime([hr, min, sec - 1]);
+    }
+  };
+
+  const reset = useCallback(
+    () =>
+      setTime([
+        parseInt(timer.hour),
+        parseInt(timer.minute),
+        parseInt(timer.second),
+      ]),
+    [timer.hour, timer.minute, timer.second]
   );
+
+  const value = useMemo(() => ({ hr, min, sec, reset }), [hr, min, sec, reset]);
 
   return (
     <TimerContext.Provider value={value}>{children}</TimerContext.Provider>
