66dc44cfdf203a6b458d7d1d5936babfa714b5ab
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-12-28 22:50:26 -0300

    chore: change top bar buttons and add drag css property

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
-  const win = new BrowserWindow({ width: 500, height: 700 });
+  const win = new BrowserWindow({
+    width: 400,
+    height: 700,
+    title: "Pomodoro",
+    titleBarStyle: "customButtonsOnHover",
+  });
   win.loadURL("http://localhost:3000");
 }
 
