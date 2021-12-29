0e493462aca4c5a58335d5ab167b829f587e51cf
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-12-28 23:13:29 -0300

    chore: remove tasks and create timer component

---
+++ src/components/Timer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+import useTimer from "@hooks/useTimer";
+
+const TimerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const TickTimer = styled.h1``;
+const SessionCount = styled.span`
+  padding: 4px 15px;
+  font-size: 11px;
+  background-color: #eeeeee;
+  border-radius: 15px;
+`;
+
+export default function Timer() {
+  const { min, sec, session } = useTimer();
+  return (
+    <TimerWrapper>
+      <TickTimer>
+        {`${min}`.padStart(2, "0")}:{`${sec}`.padStart(2, "0")}
+      </TickTimer>
+      <SessionCount>Session: {session} - Termina as 23:10</SessionCount>
+    </TimerWrapper>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,31 +2,16 @@ import React from "react";
 import Layout from "@components/Layout";
 import SessionStatus from "@components/SessionStatus";
 import ActionButtons from "@components/ActionButtons";
+import Timer from "@components/Timer";
 
 import useTimer from "@hooks/useTimer";
 
-// // Requisitos
-// A cada 1 session - 5 min de descanso
-// A cada 4 sessions - 1 stop (stop session)
-
-//  // Dados
-// Stop session - 30 min
-// Session interval - 5 min
-
-//  // Ideias extras
-// Quando estiver em tempo de working, apresentar uma animação de uma pessoa codando
-// Colocar abaixo da imagem uma barra de progresso
-// Colocar o horário que vai terminar a sessão
-
 export default function Index() {
-  const { min, sec, session, onBreak } = useTimer();
+  const { onBreak } = useTimer();
   return (
     <Layout>
       <SessionStatus onBreak={onBreak} />
-      <br />
-      Timer: {`${min}`.padStart(2, "0")}:{`${sec}`.padStart(2, "0")}
-      <br />
-      Session: {session}
+      <Timer />
       <ActionButtons />
     </Layout>
   );
